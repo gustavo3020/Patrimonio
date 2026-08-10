87e425394969a26013e7947766cc1e1c7e4daee9
gustavo3020 <gustavo-luigi@hotmail.com>
2026-08-10 07:51:53 -0300

    feat: adiciona e configura serilog

--- src/Core/Patrimonio.Api/Program.cs
+++ src/Core/Patrimonio.Api/Program.cs
@@ -1,4 +1,11 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
