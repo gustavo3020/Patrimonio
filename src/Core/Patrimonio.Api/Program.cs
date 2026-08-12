20d05ff9ae13cd54518f8d654c480fb78a8436a6
gustavo3020 <gustavo-luigi@hotmail.com>
2026-08-12 08:13:16 -0300

    feat: configurar Scalar para documentação da API

--- src/Core/Patrimonio.Api/Program.cs
+++ src/Core/Patrimonio.Api/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.MapControllers();
